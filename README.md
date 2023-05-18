6a939ca421d0c75cdb665ea2750546e341967713
Nikola Petrovic <nikola.petrovic@fer.hr>
2023-05-18 10:09:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Projekt iz predmeta APT/TAR
+
+Tema: Personality Trait Classification on Essays
+
---
+++ code/data_prep.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import nltk
+import sklearn
+
+df = pd.read_csv("./code/data/essays.csv", encoding="ansi")
+
+print(df.shape)
+print(df.head())
+
+
+
+
+
+
+
+
+
